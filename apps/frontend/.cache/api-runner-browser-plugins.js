002b35526874dfe4a10f86d48d6cf6ea4f2163be
Jan Honza Pozivil <jpozivil@leadtech.com>
2023-05-06 15:31:56 +0200

    Cleanup leftovers from NextJS test

--- apps/frontend/.cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('../../../node_modules/.pnpm/gatsby@5.9.0_babel-eslint@10.1.0_react-dom@18.2.0_react@18.2.0_typescript@5.0.4/node_modules/gatsby/dist/internal-plugins/partytown/gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
