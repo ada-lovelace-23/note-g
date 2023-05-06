4b2b5c9fb9410c633c57cd53d362d0b24c5434f4
Pedro Salas <psalas@leadtech.com>
2023-05-06 19:48:45 +0200

    remove backend folder

--- apps/backend/handler/audioHandler.js
+++ apps/backend/handler/audioHandler.js
@@ -1,79 +1,32 @@
-const AWS = require('aws-sdk');
-const dynamo = new AWS.DynamoDB.DocumentClient();
+const { LambdaClient, InvokeCommand } = require("@aws-sdk/client-lambda");
 
-const TABLE_NAME = process.env.GREETINGS_TABLE
-
-exports.saveHello = async (event) => {
+exports.audioHandler = async (event) => {
     console.log(event);
 
-    const name = event.queryStringParameters.name;
+    // a client can be shared by different commands.
+    const client = new LambdaClient({ region: "eu-west-2" });
 
-    const item = {
-        id: name,
-        name: name,
-        date: Date.now()
+    const params = {
+        FunctionName: 'note-g-transpileAudio-9vLpQRrNnQ4D',
+        InvocationType: 'RequestResponse',
+        LogType: 'None',
+        Payload: '{}',
+    };
+
+    const command = new InvokeCommand(params);
+
+    let data = null
+
+    try {
+        data = await client.send(command);
+    } catch (error) {
+        throw new Error('Failed to get response from lambda function')
     }
 
-    console.log(item);
-
-    const savedItem = await saveItem(item);
+    const response = JSON.parse(Buffer.from(data.Payload).toString());
 
     return {
         statusCode: 200,
-        body: JSON.stringify(savedItem),
-      }
-}
-
-exports.getHello = async (event) => {
-    const name = event.queryStringParameters.name;
-
-    try {
-        const item = await getItem(name);
-        console.log(item);
-    
-        if (item.date) {
-            const d = new Date(item.date);
-            
-            return {
-                statusCode: 200,
-                body: `Was greeted on ${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()}`
-            }
-        }
-    } catch (e) {
-        return {
-            statusCode: 200,
-            body: 'Nobody was greeted with that name'
-        }
+        body: JSON.stringify(response),
     }
 }
-
-async function saveItem(item) {
-    const params = {
-		TableName: TABLE_NAME,
-		Item: item
-	};
-
-    console.log(params)
-    
-    return dynamo.put(params).promise().then(() => {
-        return item;
-    });
-};
-
-async function getItem (name) {
-    console.log('getItem');
-    
-    const params = {
-      Key: {
-        id: name,
-      },
-      TableName: TABLE_NAME
-    };
-
-    console.log(params);
-  
-    return dynamo.get(params).promise().then(result => {
-        console.log(result);
-        return result.Item;
-    });
-};
