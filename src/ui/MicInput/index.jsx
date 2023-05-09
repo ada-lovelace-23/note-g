c34770e297b9d1ca29506edddfc5cf30eba37dec
Pedro Salas <psalas@leadtech.com>
2023-05-09 03:28:48 +0200

    isrecording button

--- src/ui/MicInput/index.jsx
+++ src/ui/MicInput/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './micStyles.css'
-
-const constraints = { audio: true, video: false };
+import { BsFillRecordCircleFill } from "react-icons/bs";
+import { IconContext } from "react-icons";
 
 const micButtonStyle = {
     display: 'flex',
@@ -20,37 +20,21 @@ const microphoneStyle = {
 };
 
 
-const MicDisabled = () => (
-    <>
-        <MicOff fontSize="large" />
-        <span>Mic disabled</span>
-    </>
-);
 
-const MicStop = () => (
-    <>
-        <Mic fontSize="large" /> <span>Stop</span>
-    </>
-);
+const Microphone = ({ micClickhandler, isRecording }) => {
 
-const MicRecord = () => (
-    <>
-        <MicNone fontSize="large" /> <span>Record</span>
-    </>
-);
-
-const Microphone = ({ micClickhandler }) => {
-
+    const iconValues = {
+        className: isRecording ? "recordIcon recordIcon--disable" : "recordIcon"
+    }
     return (
         <div style={microphoneStyle}>
             <button
                 onClick={micClickhandler}
                 styles={micButtonStyle}
                 className="micInputButton">
-                <svg xmlns="http://www.w3.org/2000/svg" width="136" height="136" fill="red" viewBox="0 0 16 16">
-                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                    <path d="M11 8a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
-                </svg>
+                    <IconContext.Provider value={iconValues}>
+                        <BsFillRecordCircleFill />
+                    </IconContext.Provider>
             </button>
             {/* {blobUrl && <audio controls src={blobUrl}></audio>} */}
         </div>
